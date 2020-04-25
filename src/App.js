ca4e55db949194aadc6d398b6785d79fa7180fc0
Rishav Sinha <rishav@live.co.uk>
2020-04-26 01:29:05 +0530

    Checking

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.s
+          Edit <code>src/App.js</code> and save to reload.sss
         </p>
         <a
           className="App-link"
