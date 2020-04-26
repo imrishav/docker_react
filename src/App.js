ac1f0a4c4adf77d5f66cec6c5420b78fe9a72571
Rishav Sinha <rishav@live.co.uk>
2020-04-26 15:10:33 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Rssishav Sinha.sssd...sss
+          Learn React From Rssssishav Sinha.sssd...sss
         </a>
       </header>
     </div>
